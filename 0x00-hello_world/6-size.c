6b2e55bdddc5255bf1a77353169aa06366c30160
Valentine1244 <ifeanyiakamigbo@yahoo.com>
2023-03-16 13:20:15 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  * main - A program that prints the size of various computer types
- * Return 0 {Success}
+ * Return: Always 0 {Success}
  */
 int main(void)
 {
 	char a;
 	int b;
-	long int c;
+	long c;
 	long long int d;
 	float f;
 
