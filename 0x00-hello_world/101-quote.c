2e7ce067202be8a32645d4a8156a8923daa2b033
Valentine1244 <ifeanyiakamigbo@yahoo.com>
2023-03-16 13:10:31 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A C program that prints a line to the standard error
- * Return 1 {Success}
+ * main - Entry point
+ * Return: Always 1 {Success}
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fprintf(stderr, "and that piece of art is useful\"");
+	fprintf(stderr, " - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
