951a16878c826b75ed05c040cb3b0b859672f6e6
Valentine1244 <akamigboifeanyi22@gmail.com>
2023-06-15 06:45:16 -0700

    douuble linked

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -4,16 +4,20 @@
  * @h: head node
  * Return: Size_t (number of nodes)
  */
-size_t print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h)
 {
-	dlistint_t *current;
+	
 	int count = 0;
 
-	current = h;
-	while (current != NULL)
+	if (h == NULL)
+	{
+		return (count);
+	}
+	while (h != NULL)
 	{
 		count += 1;
-		current = current->next;
+		printf("%d\n", h->n);
+		h = h->next;
 	}
 	return (count);
 }
