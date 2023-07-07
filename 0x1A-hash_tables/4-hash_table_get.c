0d3759967c1a6bb1d91d0c7f83c1bab3269b0426
Valentine1244 <akamigboifeanyi22@gmail.com>
2023-07-07 02:08:18 -0700

    prints hash table

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+#include <string.h>
+
+/**
+ * hash_chain_get - Get an element from a singly-linked list
+ * @head: A pointer to the singly-linked list
+ * @key: A pointer to the element's key
+ *
+ *
+ * Return: a pointer to the element's value, or NULL if the key wasn't found
+ */
+char *hash_chain_get(const hash_node_t *head, const char *key)
+{
+	if (head)
+	{
+		if (strcmp(key, head->key))
+			return (hash_chain_get(head->next, key));
+
+		return (head->value);
+	}
+	return (NULL);
+}
+
+/**
+ * hash_table_get - get an element from a hash table
+ * @ht: a pointer to the hash table
+ * @key: a pointer to the element's key
+ *
+ *
+ * Return: a pointer to the element's value, or NULL if the key wasn't found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = 0;
+
+	if (ht && key && *key)
+	{
+		index = key_index((const unsigned char *) key, ht->size);
+		return (hash_chain_get(ht->array[index], key));
+	}
+	return (0);
+}
